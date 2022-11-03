ece05e423f422fa040f9f19f15845a90e1a8797a
andy6170 <56803536+andy6170@users.noreply.github.com>
2022-11-03 13:46:07 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+(function () {
+    const plugin = BF2042Portal.Plugins.getPlugin("red-variables");
+
+    plugin.initializeWorkspace = function () {
+        const theme = _Blockly.getMainWorkspace().getTheme();
+
+        const variableStyle = theme.blockStyles["variable-block-style"];
+        variableStyle.colourPrimary = "#b53e3e";
+        variableStyle.colourSecondary = "#990303";
+        variableStyle.colourTertiary = "#6e0000";
+
+        _Blockly.getMainWorkspace().setTheme(theme);
+    };
+})();
