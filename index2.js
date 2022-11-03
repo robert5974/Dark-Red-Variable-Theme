18aaeb38fda89edbdfbe8d45b7ef55b02ceb9736
andy6170 <56803536+andy6170@users.noreply.github.com>
2022-11-03 15:56:13 +0000

    Update index2.js

--- index2.js
+++ index2.js
@@ -5,9 +5,9 @@
         const theme = _Blockly.getMainWorkspace().getTheme();
 
         const variableStyle = theme.blockStyles["variable-block-style"];
-        variableStyle.colourPrimary = "#5a0000"; // Body of Subroutine R - 90
+        variableStyle.colourPrimary = "#640000"; // Body of Subroutine R - 100
         variableStyle.colourSecondary = "#870000"; //
-        variableStyle.colourTertiary = "#460000"; // Edge of Subroutine r - 70
+        variableStyle.colourTertiary = "#460000"; // Edge of Subroutine R - 70
 
         _Blockly.getMainWorkspace().setTheme(theme);
     };
